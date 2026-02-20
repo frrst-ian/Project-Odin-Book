56e27ef7e2a632e82a20b8c3a313d6c1091eb754
frrst-ian <frrstxd@proton.me>
2026-02-20 10:17:46 +0800

    fix: use prod callback uri

--- config/passport.js
+++ config/passport.js
@@ -64,7 +64,7 @@ passport.use(
         {
             clientID: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            callbackURL: "http://localhost:3000/api/auth/github/callback",
+            callbackURL: "https://odinbookxd.netlify.app/api/auth/github/callback",
             scope: ["user:email"],
             customLogic: async (accessToken, profile) => {
                 const emailsResponse = await require("axios").get(
